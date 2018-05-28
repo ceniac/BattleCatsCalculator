819e026d160d2d4735cf0619da3dca550d59b55d
Christopher Nice <me@christophernice.com>
2018-05-27 21:15:53 -0500

    7.0 updates

--- BatCatTracks/Constants.cs
+++ BatCatTracks/Constants.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BatCatTracks.Models;
 
 namespace BatCatTracks
 {
-	// Updated for 6.10
+	// Updated for 7.0
 	public class Constants
 	{
 		public static readonly int Nekoluga = 215;
@@ -16,19 +12,19 @@ namespace BatCatTracks
 		public static readonly int DragonEmps = 266;
 		public static readonly int DarkHeroes = 233;
 		public static readonly int Almighties = 236;
-		public static readonly int IronLegion = 239;
+		public static readonly int IronLegion = 283;
 		public static readonly int GirlsMons = 248;
-		public static readonly int Elementals = 277;//temp for 6.10
+		public static readonly int Elementals = 277;
 		public static readonly int UltraSouls = 230;
 		public static readonly int Vajiras = 221;
 		public static readonly int RedBusters = 245;
 		public static readonly int AirBusters = 246;
 		public static readonly int MetalBusters = 269;
-		public static readonly int UberFest = 270;
-		public static readonly int EpicFest = 271;
+		public static readonly int UberFest = 286;
+		public static readonly int EpicFest = 287;
 
 		// limited:
 		public static readonly int Baseball = 265;
-		public static readonly int Eva = 276;
+		public static readonly int Eva = 282;
 	}
 }
